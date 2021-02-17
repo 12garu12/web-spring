f4af0e1b97dbcce987f7bf3fddd232e2462d33bd
garu12 <garu12@hotmail.com>
2021-02-16 20:07:42 -0500

    Creando el package models y el subpackage entity con la clase Cliente y el subpackage dao

---
+++ src/main/java/com/practice/webspring/app/models/entity/Cliente.java
@@ -0,0 +1,85 @@
+package com.practice.webspring.app.models.entity;
+
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+
+    private Long id;
+
+    @NotEmpty
+    private String firstName;
+
+    @NotEmpty
+    private String lastName;
+
+    @NotEmpty
+    @Email
+    private String email;
+
+    @NotNull
+    @Column(name = "create_at")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date createAt;
+
+    @PrePersist
+    private void prePersist(){
+        createAt = new Date();
+    }
+
+/*  Getters and Setters ***********************************************************************************************/
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    private static final Long SerialVersionUID = 1L;
+}
