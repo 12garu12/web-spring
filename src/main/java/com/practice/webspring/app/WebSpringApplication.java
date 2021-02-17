dfe4bcfbfc0a95ade77e1486631e2a0c2bf9a167
garu12 <garu12@hotmail.com>
2021-02-16 19:05:51 -0500

    Inicio del proyecto con toda la estructura con maven

---
+++ src/main/java/com/practice/webspring/app/WebSpringApplication.java
@@ -0,0 +1,13 @@
+package com.practice.webspring.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/webspring/app/WebSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.webspring.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
